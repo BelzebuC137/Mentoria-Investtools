0ca26ee219c5f7562be129e654b0717bd3af5c1d
vinicius <134892899+BelzebuC137@users.noreply.github.com>
2023-10-21 11:01:21 -0300

    first-commit

---
+++ treino.js
@@ -0,0 +1,7 @@
+function main(){
+   // input = document.getElementById('input1');
+    //console.log('my name is'+'' + input.value)
+   for(i=0; i<=10; i++){
+console.log('6x ' +i+ '=' +i*6)   
+}
+   }
